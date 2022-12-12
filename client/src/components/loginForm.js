9857781f6dc585eafae372c5233432dee798ba4d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-12 10:17:27 -0500

    updated the imports for loginForms js file

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -1,8 +1,9 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
