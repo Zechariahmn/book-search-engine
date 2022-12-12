fe780bfc82896add78d9eb31979c664253769dae
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-12 10:23:38 -0500

    final loginForm.js file

--- client/src/components/loginForm.js
+++ client/src/components/loginForm.js
@@ -54,7 +54,8 @@ const LoginForm = () => {
       const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
-    } catch (err) {
+    } 
+    catch (err) {
       console.error(err);
       setShowAlert(true);
     }
@@ -69,11 +70,15 @@ const LoginForm = () => {
   return (
     <>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your login credentials!
         </Alert>
+
         <Form.Group>
+
           <Form.Label htmlFor='email'>Email</Form.Label>
+
           <Form.Control
             type='text'
             placeholder='Your email'
@@ -82,11 +87,15 @@ const LoginForm = () => {
             value={userFormData.email}
             required
           />
+
           <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+
         </Form.Group>
 
         <Form.Group>
+
           <Form.Label htmlFor='password'>Password</Form.Label>
+
           <Form.Control
             type='password'
             placeholder='Your password'
@@ -95,15 +104,20 @@ const LoginForm = () => {
             value={userFormData.password}
             required
           />
+
           <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+
         </Form.Group>
+
         <Button
           disabled={!(userFormData.email && userFormData.password)}
           type='submit'
           variant='success'>
           Submit
         </Button>
+
       </Form>
+      
     </>
   );
 };
