0715671388aeea5e50eafa27e4d3697505a0d93b
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-12 10:10:32 -0500

    updated imports for App js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
