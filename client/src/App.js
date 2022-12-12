0d863b865f862371ec10be46a6dcaae30d9e9af6
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-12 10:12:24 -0500

    added link for localstorage

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,16 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 function App() {
   return (
     <Router>
