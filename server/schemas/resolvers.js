fcec4d40746ad22705b8549fb9398ee62ff400f1
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-13 10:56:29 -0500

    added query password

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+    },
