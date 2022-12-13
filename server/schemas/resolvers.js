9a2000533ab1af01eaa39d07eefb9a29da4275b9
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-13 10:57:28 -0500

    added mutation for adding a user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,3 +13,11 @@ const resolvers = {
             throw new AuthenticationError('Not logged in');
         }
     },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
