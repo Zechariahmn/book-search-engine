47a641d2623bfebeb18bb1908cfc39ac594dbe44
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-13 17:29:18 -0500

    added changes to imports in App js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import pages
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-//const routes = require('./routes');
 
+
+//const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
