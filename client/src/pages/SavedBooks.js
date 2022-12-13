4f812d218562feea5b89e9507393f5ea70e8c7b5
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-13 10:33:30 -0500

    changes to saved books js file

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -19,3 +19,89 @@ const SavedBooks = () => {
       </h4>
     );
   }
+
+   // create function that accepts the book's mongo _id value as param and deletes the book from the database
+   const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      await deleteBook({
+        variables: {bookId: bookId},
+        update: cache => {
+          const data = cache.readQuery({ query: GET_ME });
+          const userDataCache = data.me;
+          const savedBooksCache = userDataCache.savedBooks;
+          const updatedBookCache = savedBooksCache.filter((book) => book.bookId !== bookId);
+          data.me.savedBooks = updatedBookCache;
+          cache.writeQuery({ query: GET_ME , data: {data: {...data.me.savedBooks}}})
+        }
+      });
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    } 
+  catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+
+      </Jumbotron>
+
+      <Container>
+
+        <h2>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+
+        <CardColumns>
+
+          {userData.savedBooks.map((book) => {
+            return (
+
+              <Card key={book.bookId} border='dark'>
+                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card.Body>
+
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className='small'>Authors: {book.authors}</p>
+                  {book.link ? <Card.Text><a href={book.link} target="_blank">More Information on Google Books</a></Card.Text> : null}
+                  <Card.Text>{book.description}</Card.Text>
+
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    Delete this Book
+                  </Button>
+
+                </Card.Body>
+
+              </Card>
+
+            );
+          })}
+          
+        </CardColumns>
+
+      </Container>
+
+    </>
+  );
+};
+
+export default SavedBooks;
