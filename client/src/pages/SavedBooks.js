7c94f2f7ff452a498173b8ce1721d579a2668939
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-13 10:31:53 -0500

    added a get and delete function for saved books js file

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,3 +6,16 @@ import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+
+const SavedBooks = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
+  const userData = data?.me || {};
+
+  if(!userData?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
