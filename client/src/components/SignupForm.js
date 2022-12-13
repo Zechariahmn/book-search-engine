d823cca8c6309d3663629187b527f25c7a7299d6
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-13 17:41:41 -0500

    made changes signupForm js file

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -36,18 +36,14 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } 
-  catch (err) {
-      console.error(err);
+  catch (e) {
+      console.error(e);
       setShowAlert(true);
     }
 
