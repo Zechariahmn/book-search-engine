772a7248cecd8a13cd4b04e1a6027cb90e8fb4ba
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-13 17:36:18 -0500

    added changes to the imports in signupForm js file

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
