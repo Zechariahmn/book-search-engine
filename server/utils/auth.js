e84bc085592113521607b02a5dc17e459ceec70d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-14 12:17:57 -0500

    created a seeds folder with corresponding files, and made changes to auth js file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'mysecrets';
 const expiration = '2h';
 
 module.exports = {
+
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
+
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -23,14 +25,16 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+    } 
+  catch {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
     next();
-  },
+  };
+  
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
