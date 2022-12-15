f0330d5ebf35ca9a3da9b7c003d5eee9df412d33
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-15 14:11:07 -0500

    made changes to imports in files and deleted build folde

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,5 +1,5 @@
 const db = require('../config/connection');
-const { User } = require('../models/User');
+const { User } = require('../models/');
 const userSeeds = require('./userSeeds.json');
 
 db.once('open', async () => {
--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./schemas/index');
+const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
