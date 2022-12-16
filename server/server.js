eba8aa50aa6ccb58d3249110e95cc402148c1489
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-16 10:10:45 -0500

    updating package jsons

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
